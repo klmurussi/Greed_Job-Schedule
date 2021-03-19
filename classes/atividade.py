30d93ca32c73b0e0d676990d0c1f32bacdd52346
jschneiderm98 <julio_schneider@hotmail.com>
2021-03-18 22:41:09 -0300

    ordenar array pelo horario inicial
    Co-authored-by: klmurussi <klmurussi@hotmail.com>

--- classes/atividade.py
+++ classes/atividade.py
@@ -1,11 +1,15 @@
 from classes import dias
 
-class Atividade :
-	def __init__(self, nome, horarioInicial, horarioFinal, days):	
-		self.nome = nome
-		self.horarioInicial = horarioInicial
-		self.horarioFinal = horarioFinal
-		self.dias = days
 
-	def print (self):
-		print ("my name is " + self.nome)
+class Atividade:
+    def __init__(self, nome, horarioInicial, horarioFinal, days):
+        self.nome = nome
+        self.horarioInicial = horarioInicial
+        self.horarioFinal = horarioFinal
+        self.dias = days
+
+    def __repr__(self):
+        return repr(("nome: " + self.nome + " horario incial: " + str(self.horarioInicial)))
+
+    def print(self):
+        print("my name is " + self.nome)
--- classes/tudo.py
+++ classes/tudo.py
@@ -1,13 +1,14 @@
 from classes import atividade
 
+
 class Tudo:
-	def __init__(self):
-		self.atividades = []
+    def __init__(self):
+        self.atividades = []
 
-	def add (self, atividade):
-		self.atividades.append(atividade)
+    def add(self, atividade):
+        self.atividades.append(atividade)
 
-	def print (self):
-		for n in self.atividades:
-			print (n.nome)
-			print (n.horarioInicial)
+    def print(self):
+        for n in self.atividades:
+            print(n.nome, "horario inicial:", int(n.horarioInicial),
+                  "horario final:", int(n.horarioFinal))
